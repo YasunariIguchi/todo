10257a0002a7af6fda1d6c65690ed983617900bd
YasunariIguchi <iguchi.yasunari.0219@gmail.com>
2023-11-24 16:40:51 +0900

    breeze APIインストール

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TodoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -10,17 +9,11 @@ use App\Http\Controllers\TodoController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
-
-// ToDoのCRUD
-Route::post('todo', [TodoController::class, 'create']);
-Route::get('todo', [TodoController::class, 'fetch']);
-Route::put('todo', [TodoController::class, 'update']);
-Route::delete('todo', [TodoController::class, 'delete']);
